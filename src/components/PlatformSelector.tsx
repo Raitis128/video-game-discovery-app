6425c035d41127b9f2cab642214fba9bd8d3ad2c
Raitis <raitisiziks@gmail.com>
2024-10-24 16:50:29 +0300

    Refactor usePlatforms hook

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -9,7 +9,6 @@ interface Props {
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  
   const { data, error } = usePlatforms();
 
   if (error) return null;
@@ -20,7 +19,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
